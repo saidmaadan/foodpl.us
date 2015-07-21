da04f4c1811ca1453718cd1e0af2ca6cc0514a21
Said Maadan <saidmaadan@users.noreply.github.com>
2015-07-20 21:00:27 -0500

    Modified user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,19 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :timeoutable
-  has_many :reviews, dependent: :destroy
-  has_many :food_places, dependent: :destroy
+  	has_many :reviews, dependent: :destroy
+  	has_many :food_places, dependent: :destroy
 
-  validates :first_name, :last_name, presence: true
-  validates :username, presence: true,
+  	validates :first_name, :last_name, presence: true
+  	validates :username, presence: true,
                      format: /\A[A-Z0-9]+\z/i,
                      uniqueness: { case_sensitive: false }
+
+
+    def should_generate_new_friendly_id?
+    	new_record?
+  	end
 
 end
