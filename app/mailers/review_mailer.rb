ef02145dd149be529effbf109280012c212dc424
Said Maadan <saidmaadan@users.noreply.github.com>
2015-07-20 20:13:38 -0500

    Rectified typo in review mailer

--- app/mailers/review_mailer.rb
+++ app/mailers/review_mailer.rb
@@ -1,13 +1,14 @@
 class ReviewMailer < ActionMailer::Base
 
-	def review_created(current_user, food_place.user, comment)
+	def review_created(current_user, food_place_user, comment)
 
 		@current_user = current_user
-		@food_place.user = food_place.user
+		@food_place_user = food_place_user
 		@comment = comment
 
-		mail(to: food_place.user.email,
+		mail(to: food_place_user.email,
 			from: "hr@foodpl.us",
-			subject: "Review Created")
+			subject: "Review Created",
+		)
 	end
 end
